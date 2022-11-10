d3df55152d90d39e96c352b1fbf6bf76fefaf105
Daniel <darodriguez@mundonectar.com>
2022-11-10 09:40:21 -0600

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -12,20 +12,18 @@ let package = Package(
             name: "Shimmer",
             targets: ["FBShimmering"]),
     ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
-    ],
+//    dependencies: [
+//        // Dependencies declare other packages that this package depends on.
+//        // .package(url: /* package url */, from: "1.0.0"),
+//    ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "FBShimmering",
-            path: "FBShimmering,
             publicHeadersPath: "include"
-            dependencies: []),
-        .testTarget(
-            name: "ShimmerTests",
-            dependencies: ["Shimmer"]),
-    ]
+        )
+        
+    ],
+    swiftLanguageVersions: [.v5]
 )
